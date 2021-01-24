f44a9a0ff4be5a1667c1ae8da918e7d8d2524977
BlueEnni <enricobachus@t-online.de>
2021-01-24 20:29:53 +0100

    new frontend

--- frontend/src-new/app/app.module.ts
+++ frontend/src-new/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//custom
+//for http requests
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+//html components via sudo ng g c <name>
 import { HomeComponent } from './home/home.component';
 import { TestComponent } from './test/test.component';
 import { NotFoundErrorComponent } from './not-found-error/not-found-error.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -25,7 +34,11 @@ import {ReactiveFormsModule} from '@angular/forms';
     BrowserAnimationsModule,
     MatInputModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
